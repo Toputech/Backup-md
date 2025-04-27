cd8fac533f5bdd1bb43297839aa8cc810d1cddb2
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-27 11:22:56 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -1,47 +1,72 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/Toputech/ALONE-MD-V1';
-  const img = 'https://i.imgur.com/jE8eQsP.jpeg';
 
-  try {
-    const response = await fetch(githubRepo);
-    const data = await response.json();
 
-    if (data) {
-      const repoInfo = {
-        stars: data.stargazers_count,
-        forks: data.forks_count,
-        lastUpdate: data.updated_at,
-        owner: data.owner.login,
-      };
 
-      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
-      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `🫣Ｈｕｒｒａａｈ..!!
-      this is •𝘼𝙇𝙊𝙉𝙀-𝙈𝘿•
-      ᴏᴜʀ ᴍɪssɪᴏɴ ɪs ᴛᴏ ʟᴇᴛ ʏᴏᴜ ᴇɴᴊᴏʏ ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ.... ɪ ʟᴏᴠᴇ ʏᴏᴜ 💘'
-      \n support : ,  https://whatsapp.com/channel/0029VaeRrcnADTOKzivM0S1r
 
-🗼 *REPOSITORY:* ${data.html_url}
-🌟 *STARS:* ${repoInfo.stars}
-🧧 *FORKS:* ${repoInfo.forks}
-📅 *RELEASE DATE:* ${releaseDate}
-🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
-👨‍💻 *OWNER:* *Topu tech*
-💞 *THEME:* *TOPU*
-🥰 *𝚂𝚒𝚗𝚌𝚎 𝚢𝚘𝚞 𝚑𝚊𝚟𝚎 𝚖𝚎, 𝚢𝚘𝚞'𝚕𝚕 𝚗𝚎𝚟𝚎𝚛 𝚏𝚎𝚎𝚕 𝚊𝚕𝚘𝚗𝚎 !* ❣️
-__________________________________
-            * ❣️ ©Toputech* `;
 
-      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
-    } else {
-      console.log("Could not fetch data");
+
+
+
+
+
+
+const util = require('util');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const os = require("os");
+const moment = require("moment-timezone");
+const conf = require(__dirname + "/../set");
+
+const AUDIO_URL = "https://files.catbox.moe/hrf957.mp3"; // New audio URL
+const THUMBNAIL_URL = "https://files.catbox.moe/yedfbr.jpg"; // New image URL
+
+moment.tz.setDefault(`${conf.TZ}`);
+
+const getTimeAndDate = () => {
+    return {
+        time: moment().format('HH:mm:ss'),
+        date: moment().format('DD/MM/YYYY')
+    };
+};
+
+// Ping Command
+zokou({ nomCom: "repo", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms } = commandeOptions;
+    const { time, date } = getTimeAndDate();
+    const ping = Math.floor(Math.random() * 1000) + 1; // Generate a random ping between 1ms - 100ms
+
+    try {
+    await zk.sendMessage(dest, {
+        audio: { url: AUDIO_URL }, 
+            mimetype: 'audio/mp4', 
+            ptt: true, // Voice note form
+      text: ` Current speed of ALONE MD BOT is.: ${ping}ms\n🍒🍓`,
+      contextInfo: {
+        forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: '120363295141350550@newsletter',
+              newsletterName: 'ALONE Queen MD V²',
+              serverMessageId: 143},
+        externalAdReply: {
+          
+          title: "Follow for updates 🩸",
+      body: "Enjoy...",
+      thumbnailUrl: conf.URL,
+          sourceUrl: conf.GURL,
+          mediaType: 1,
+          
+        }
+      }
+    }, { quoted: ms });
+
+    await zk.sendMessage(dest, {
+        text: "```🍒repository link available at our official channel ...follow and grab yours🎈```"
+    } ,{ quoted: ms });// Voice note form
+    }catch (e) {
+        console.log("❌ Ping Command Error: " + e);
+        repondre("❌ Error: " + e);
     }
-  } catch (error) {
-    console.log("Error fetching data:", error);
-  }
 });
