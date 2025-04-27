e67e2143cc11d3419e8ce4b075bf0824ad1c01f5
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-27 11:45:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1372,11 +1372,9 @@ if (conf.AUTO_LIKE_STATUS === "yes") {
                 await activateCrons();
 
                 if((conf.DP).toLowerCase() === 'no') {     
-                    const response = await axios.get("https://nekos.life/api/v2/fact");
-    const data = response.data;
                 let cmsg = `ALONE-MD  RUNNING ...
     
-    FACT:  ${data.fact} 
+    Prefix : [ ${prefixe} ]
     Mode :${md} mode
     Total Command : ${evt.cm.length}
     Owner : TOPU TECH 🦸  
