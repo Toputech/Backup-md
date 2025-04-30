bf26cffe79d4cccf986326fafa3594c7a4ec625e
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-30 17:32:04 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of boniphace md bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_Alone md is active since: ${runtime(process.uptime())} ago🏆_*`) 
 
    
 
