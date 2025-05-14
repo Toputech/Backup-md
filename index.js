0fcc2dc697bfc8e8c5364185412f0107e4f28142
Toputech <168991681+Toputech@users.noreply.github.com>
2025-05-14 07:53:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1260,6 +1260,52 @@ zk.ev.on('group-participants.update', async (group) => {
 ${metadata.desc}\n\n> POWERED BY TOPU TECH.`;
 
             zk.sendMessage(group.id, { image: { url: ppgroup }, caption: msg, mentions: membres });
+            const audioFiles = [
+    'https://files.catbox.moe/hpwsi2.mp3',
+    'https://files.catbox.moe/xci982.mp3',
+    'https://files.catbox.moe/utbujd.mp3',
+    'https://files.catbox.moe/w2j17k.m4a',
+    'https://files.catbox.moe/851skv.m4a',
+    'https://files.catbox.moe/qnhtbu.m4a',
+    'https://files.catbox.moe/lb0x7w.mp3',
+    'https://files.catbox.moe/efmcxm.mp3',
+    'https://files.catbox.moe/gco5bq.mp3',
+    'https://files.catbox.moe/26oeeh.mp3',
+    'https://files.catbox.moe/a1sh4u.mp3',
+    'https://files.catbox.moe/vuuvwn.m4a',
+    'https://files.catbox.moe/wx8q6h.mp3',
+    'https://files.catbox.moe/uj8fps.m4a',
+    'https://files.catbox.moe/dc88bx.m4a',
+    'https://files.catbox.moe/tn32z0.m4a'
+  ];
+
+  // Randomly pick an audio file from the list
+  const selectedAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+  // Audio message object
+  const audioMessage = {
+    audio: {
+      url: selectedAudio,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,  // Marking this as a "Push-to-Talk" message
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'shizo',
+    contextInfo: {
+      externalAdReply: {
+        title: 'ENJOY IN OUR GROUP...',
+        body: conf.OWNER_NAME,
+        thumbnailUrl: conf.URL,
+        sourceUrl: conf.GURL, // Corrected variable name
+        mediaType: 1,
+        renderLargerThumbnail: false,
+      },
+    },
+  };
+
+  // Send the audio message with the context of the original message
+  await zk.sendMessage(dest, audioMessage, );
+            
         } else if (group.action == 'remove' && (await recupevents(group.id, "goodbye") == 'on')) {
             let msg = `one or somes member(s) left group;\n`;
 
