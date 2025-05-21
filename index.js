86fbb58399804217211b8115f363256bb2ead417
Toputech <168991681+Toputech@users.noreply.github.com>
2025-05-21 15:05:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1246,55 +1246,8 @@ zk.ev.on('group-participants.update', async (group) => {
 ◇ *WElOME BUDDY... READ THE GRPUP DESCRIPTION 😊 *   ◇
 
 ${metadata.desc}\n\n> POWERED BY TOPU TECH.`;
-            
-  // Array of sound file URLs
-  const audioFiles = [
-    'https://files.catbox.moe/hpwsi2.mp3',
-    'https://files.catbox.moe/xci982.mp3',
-    'https://files.catbox.moe/utbujd.mp3',
-    'https://files.catbox.moe/w2j17k.m4a',
-    'https://files.catbox.moe/851skv.m4a',
-    'https://files.catbox.moe/qnhtbu.m4a',
-    'https://files.catbox.moe/lb0x7w.mp3',
-    'https://files.catbox.moe/efmcxm.mp3',
-    'https://files.catbox.moe/gco5bq.mp3',
-    'https://files.catbox.moe/26oeeh.mp3',
-    'https://files.catbox.moe/a1sh4u.mp3',
-    'https://files.catbox.moe/vuuvwn.m4a',
-    'https://files.catbox.moe/wx8q6h.mp3',
-    'https://files.catbox.moe/uj8fps.m4a',
-    'https://files.catbox.moe/dc88bx.m4a',
-    'https://files.catbox.moe/tn32z0.m4a'
-  ];
 
-  // Randomly pick an audio file from the list
-  const selectedAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
-
-  // Audio message object
-  const audioMessage = {
-    audio: {
-      url: selectedAudio,
-    },
-    mimetype: 'audio/mpeg',
-    ptt: true,  // Marking this as a "Push-to-Talk" message
-    waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: 'shizo',
-    contextInfo: {
-      externalAdReply: {
-        title: 'SOMEONE HAS JUST JOINED US LETS PARTY 🥳🎉',
-        body: conf.OWNER_NAME,
-        thumbnailUrl: conf.URL,
-        sourceUrl: conf.GURL, // Corrected variable name
-        mediaType: 1,
-        renderLargerThumbnail: true,
-      },
-    },
-  };
-
-  // Send the audio message with the context of the original message
-  await zk.sendMessage(group.id, audioMessage, );
-
-            zk.sendMessage(group.id, caption: msg, mentions: membres });
+            zk.sendMessage(group.id, { image: { url: ppgroup }, caption: msg, mentions: membres });
         } else if (group.action == 'remove' && (await recupevents(group.id, "goodbye") == 'on')) {
             let msg = `one or somes member(s) left group;\n`;
 
