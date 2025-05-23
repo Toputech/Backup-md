b0b456f88c7b5dffa13e967b16da2ca6b4121f2d
Toputech <168991681+Toputech@users.noreply.github.com>
2025-05-23 12:09:05 +0300

    Update remove.js

--- commandes/remove.js
+++ commandes/remove.js
@@ -1,104 +1,65 @@
-const fs = require("fs");
-const { Sticker, StickerTypes } = require("wa-sticker-formatter");
-const conf = require("./set");
+const { zokou } = require("../framework/zokou");
 
-const stickers = [
-  "https://example.com/sticker1.webp",
-  "https://example.com/sticker2.webp"
-];
+zokou({
+  nomCom: "sendall",
+  categorie: "Group",
+  reaction: "✉️",
+}, async (jid, sock, data) => {
+  const { ms, arg, isGroup, groupMetadata } = data;
 
-zokou({ nomCom: "remove", categorie: "Group", reaction: "👨🏿‍💼" }, async (dest, zk, commandeOptions) => {
-  let {
-    msgRepondu,
-    infosGroupe,
-    auteurMsgRepondu,
-    verifGroupe,
-    nomAuteurMessage,
-    auteurMessage,
-    superUser,
-    idBot
-  } = commandeOptions;
-
-  const context = {
-    forwardingScore: 999,
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363295141350550@newsletter",
-      newsletterName: "ALONE Queen MD V²",
-      serverMessageId: 143
-    },
-    externalAdReply: {
-      title: "Fun Fact",
-      body: "Here's a fun fact to enlighten your day!",
-      thumbnailUrl: conf.URL,
-      sourceUrl: conf.GURL,
-      mediaType: 1
-    }
-  };
-
-  const send = (text) => zk.sendMessage(dest, { text, contextInfo: context });
-
-  if (!verifGroupe) return send("for groups only");
-
-  let membresGroupe = await infosGroupe.participants;
-
-  const isMember = (user) => membresGroupe.some((m) => m.id === user);
-  const getAdmins = (membres) => membres.filter((m) => m.admin).map((m) => m.id);
-
-  const adminList = getAdmins(membresGroupe);
-  const isTargetAdmin = adminList.includes(auteurMsgRepondu);
-  const isBotAdmin = adminList.includes(idBot);
-  const isAuthorAdmin = adminList.includes(auteurMessage);
-  const isTargetInGroup = isMember(auteurMsgRepondu);
-
-  try {
-    if (isAuthorAdmin || superUser) {
-      if (msgRepondu) {
-        if (isBotAdmin) {
-          if (isTargetInGroup) {
-            if (!isTargetAdmin) {
-              const stickerUrl = stickers[Math.floor(Math.random() * stickers.length)];
-              const sticker = new Sticker(stickerUrl, {
-                pack: "ALONE-MD",
-                author: nomAuteurMessage,
-                type: StickerTypes.FULL,
-                categories: ["🤩", "🎉"],
-                id: "12345",
-                quality: 50,
-                background: "#000000"
-              });
-
-              await sticker.toFile("st.webp");
-
-              const txt = `@${auteurMsgRepondu.split("@")[0]} was removed from the group.\n`;
-
-              await zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "remove");
-
-              await zk.sendMessage(dest, {
-                text: txt,
-                mentions: [auteurMsgRepondu],
-                contextInfo: context
-              });
-
-              await zk.sendMessage(dest, {
-                sticker: fs.readFileSync("st.webp")
-              }, { quoted: msgRepondu });
-            } else {
-              return send("This member cannot be removed because he is an administrator of the group.");
-            }
-          } else {
-            return send("This user is not part of the group.");
+  const replyWithContext = (text) =>
+    sock.sendMessage(jid, {
+      text,
+      contextInfo: {
+        externalAdReply: {
+          title: "ALONE MD V²",
+          body: "Group Broadcast",
+          mediaType: 1,
+          thumbnailUrl: "https://i.ibb.co/album/thumbnail.jpg", // optional
+          sourceUrl: "https://youtube.com/@alone-bot",
+          renderLargerThumbnail: true,
+          showAdAttribution: false,
+          forwardingScore: 999,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363295141350550@newsletter',
+            newsletterName: 'ALONE MD V²',
+            serverMessageId: 143
           }
-        } else {
-          return send("Sorry, I cannot perform this action because I am not an administrator of the group.");
-        }
-      } else {
-        return send("Please tag the member to be removed.");
-      }
-    } else {
-      return send("Sorry, I cannot perform this action because you are not an administrator of the group.");
-    }
-  } catch (e) {
-    return send("Oops " + e);
+        },
+      },
+    }, { quoted: ms });
+
+  if (!isGroup) return replyWithContext("This command can only be used in a group.");
+  if (!arg[0]) return replyWithContext("Please provide a message to send.");
+  if (!groupMetadata) return replyWithContext("Couldn't fetch group data.");
+
+  const textToSend = arg.join(" ");
+  const senderId = ms.pushName || "Someone";
+
+  const members = groupMetadata.participants.map(p => p.id).filter(id => id !== sock.user.id);
+
+  for (const member of members) {
+    await sock.sendMessage(member, {
+      text: `*Message from group ${groupMetadata.subject}*\n\n${textToSend}`,
+      contextInfo: {
+        externalAdReply: {
+          title: "Group Broadcast",
+          body: `From ${senderId}`,
+          mediaType: 1,
+          renderLargerThumbnail: true,
+          showAdAttribution: false,
+          forwardingScore: 999,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363295141350550@newsletter',
+            newsletterName: 'ALONE MD V²',
+            serverMessageId: 143
+          }
+        },
+      },
+    }).catch(err => console.log(`Failed to message ${member}:`, err));
   }
+
+  return replyWithContext(`Sent message to ${members.length} members.`);
 });
